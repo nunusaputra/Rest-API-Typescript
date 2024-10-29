7a37120b4e926c57bcc1af9f72585920a64412ff
nunusaputra <2010631170126@student.unsika.ac.id>
2024-10-29 10:33:05 +0700

    Setup EsLint, Prettier, and Husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 const port: number = 5000
 
 app.use('/', (req: Request, res: Response) => {
-  res.status(200).send({ status: '200' })
+  res.status(200).send({ status: '200', message: 'Hello World' })
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
